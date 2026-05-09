e477ac79bd192c16adbda476c7342e9847c63078
mariots <mario_ts@outlook.com>
2026-05-08 20:50:58 -0600

    Add middleware to webhook routes for message handling and testing

--- src/routes/webhook.routes.ts
+++ src/routes/webhook.routes.ts
@@ -1,12 +1,27 @@
 import { Router } from "express";
 import {
   webhookMessageController,
+  webhookMessageTestController,
   webhookVerifyController,
 } from "../controllers/webhook.controller";
+import { parseMessageMiddleware } from "../middleware/parseMessage.middleware";
+import { handleMessageMiddleware } from "../middleware/handleMessage.middleware";
 
 const router = Router();
 
 router.get("/webhook", webhookVerifyController);
-router.post("/webhook", webhookMessageController);
+router.post(
+  "/webhook",
+  handleMessageMiddleware,
+  parseMessageMiddleware,
+  webhookMessageController,
+);
+
+router.post(
+  "/webhook/test",
+  handleMessageMiddleware,
+  parseMessageMiddleware,
+  webhookMessageTestController,
+);
 
 export default router;
