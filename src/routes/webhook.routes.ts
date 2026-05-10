78221d865d7f81a2992dd3794b175a29c16909c6
mariots <mario_ts@outlook.com>
2026-05-09 21:57:18 -0600

    Delete middleware

--- src/routes/webhook.routes.ts
+++ src/routes/webhook.routes.ts
@@ -5,21 +5,14 @@ import {
   webhookVerifyController,
 } from "../controllers/webhook.controller";
 import { parseMessageMiddleware } from "../middleware/parseMessage.middleware";
-import { handleMessageMiddleware } from "../middleware/handleMessage.middleware";
 
 const router = Router();
 
 router.get("/webhook", webhookVerifyController);
-router.post(
-  "/webhook",
-  handleMessageMiddleware,
-  parseMessageMiddleware,
-  webhookMessageController,
-);
+router.post("/webhook", parseMessageMiddleware, webhookMessageController);
 
 router.post(
   "/webhook/test",
-  handleMessageMiddleware,
   parseMessageMiddleware,
   webhookMessageTestController,
 );
